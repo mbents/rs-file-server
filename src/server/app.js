36d4b327fdf1673aa7dd4c6e078a0bdc636b2a8d
mbents <mtbents@gmail.com>
2018-08-03 20:25:19 -0500

    upgrade csvtojson version; add package-lock.json

--- src/server/app.js
+++ src/server/app.js
@@ -30,35 +30,22 @@ app.get('/file', function (req, res) {
 // Returns an array of JSON objects for a BGAME file
 // This data can be used to create a boxscore
 app.get('/boxscore', function (req, res) {
-    var Converter = require("csvtojson").Converter;
-    var converter = new Converter({
+    const csv = require("csvtojson");
+    csv({
         noheader: true,
         headers: getBGAMEHeaders()
-    });
-    
-    fs.readFile(req.query.f, function (err, data) {
-        if (err) {
-            console.log('read file error occurred');
-            console.log(err);
-            res.writeHead(500, { 'Content-Type': 'text/plain' });
-            res.end('read file failed');
-        } else {
-            var jsonStr = data.toString();
-            converter.fromString(jsonStr, function (csv_err, result) {
-                if (csv_err) {
-                    console.log('Convert error occurred');
-                    console.log(csv_err);
-                    res.writeHead(500, { 'Content-Type': 'text/plain' });
-                    res.end('convert failed');
-                }
-
-                res.writeHead(200, { 'Content-Type': 'application/json' });
-                res.write(JSON.stringify(result));
-                res.end();
-            });
-        }
-        
-    });
+    })
+    .fromFile(req.query.f)
+    .then((jsonObj) => {
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+        res.write(JSON.stringify(jsonObj));
+        res.end();
+    }, (err) => {
+        console.log('read file error occurred');
+        console.log(err);
+        res.writeHead(500, { 'Content-Type': 'text/plain' });
+        res.end('read file failed');
+    })
 });
 
 app.get('/readfile', function (req, res) {
