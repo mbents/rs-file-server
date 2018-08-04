94a2cfecad41abcbf599579d7bc4fd9920279ce0
mbents <mtbents@gmail.com>
2018-08-03 21:56:50 -0500

    remove unused endpoints

--- src/server/app.js
+++ src/server/app.js
@@ -14,20 +14,6 @@ app.get('/', function (req, res) {
     res.send('Hello World!')
 })
 
-app.get('/file', function (req, res) {
-    fs.readFile(req.query.f, function (err, data) {
-        if (err) {
-            console.log(err)
-            res.writeHead(500)
-            res.end('failed')
-        } else {
-            res.writeHead(200, { 'Content-Type': 'application/json' })
-            res.write(JSON.stringify(data))
-            res.end()
-        }
-    })
-})
-
 // Returns an array of JSON objects for a BGAME file
 // This data can be used to create a boxscore
 app.get('/boxscore', function (req, res) {
@@ -65,35 +51,6 @@ app.get('/bevent', function (req, res) {
         console.log(err)
         res.writeHead(500, { 'Content-Type': 'text/plain' })
         res.end('read file failed')
-    })
-})
-
-app.get('/readfile', function (req, res) {
-    var Converter = require("csvtojson").Converter
-    var converter = new Converter({
-        noheader: false
-    })
-
-    fs.readFile(req.query.f, function (err, data) {
-        if (err) {
-            console.log(err)
-            res.writeHead(500)
-            res.end('failed')
-        } else {
-            var jsonStr = data.toString()
-            converter.fromString(jsonStr, function (csv_err, result) {
-                if (csv_err) {
-                    console.log('Convert error occurred')
-                    console.log(csv_err)
-                    res.writeHead(500, { 'Content-Type': 'text/plain' })
-                    res.end('convert failed')
-                }
-
-                res.writeHead(200, { 'Content-Type': 'application/json' })
-                res.write(JSON.stringify(result))
-                res.end()
-            })
-        }
     })
 })
 
